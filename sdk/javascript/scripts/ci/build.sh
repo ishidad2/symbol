ecc577a6bd89491f09d9e08204ffc67f8c201b39
Jaguar0625 <jaguar0625@127.0.0.1>
2023-04-14 17:45:53 -0400

    add missing flag

--- sdk/javascript/scripts/ci/build.sh
+++ sdk/javascript/scripts/ci/build.sh
@@ -6,8 +6,8 @@ bash scripts/run_catbuffer_generator.sh dryrun
 
 cd wasm
 rustup default stable
-wasm-pack build --release --target nodejs --out-dir ../_build/wasm/node
-wasm-pack build --release --target web --out-dir ../_build/wasm/web
+wasm-pack build --release --no-typescript --target nodejs --out-dir ../_build/wasm/node
+wasm-pack build --release --no-typescript --target web --out-dir ../_build/wasm/web
 cd ..
 
 npm run bundle
